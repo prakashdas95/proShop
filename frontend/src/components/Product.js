b2e74160e67c911e3b7582e9e3b7ebb1f90be789
Prakash Das <prakashindi01@gmail.com>
2021-04-25 23:03:30 +0530

    implemented React router and productdetails screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
-import Rating from './Rating'
+import Rating from './Rating';
 
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
-                </a>
+                </Link>
                 <Card.Text as='div'>
                     <Rating
                         value={product.rating}
@@ -22,7 +23,7 @@ const Product = ({ product }) => {
                 <Card.Text as='h3'>${product.price}</Card.Text>
             </Card.Body>
         </Card>
-    )
-}
+    );
+};
 
 export default Product;
