aec691311200994e3b3c050f7f9d8758f8a367f4
Prakash Das <prakashindi01@gmail.com>
2021-04-26 21:23:46 +0530

    backend setup

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -66,13 +66,13 @@ const Rating = ({ value, text, color }) => {
 
 Rating.defaultProps = {
     color: '#f8e825'
-}
+};
 
 Rating.propTypes = {
     value: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     color: PropTypes.string,
-}
+};
 
 
 export default Rating;
