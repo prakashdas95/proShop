913bcb83df37e8d43e9ec8c0f9f6ca39d0421dba
Prakash Das <prakashindi01@gmail.com>
2021-04-27 18:27:56 +0530

    database connected , models created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -20,4 +23,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold));
