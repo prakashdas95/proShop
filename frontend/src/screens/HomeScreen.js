83d38df79906c4b94f9899f38bfb13a7571d6b0c
Prakash Das <prakashindi01@gmail.com>
2021-04-28 23:15:52 +0530

    single peoductDetails - redux store

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
-import Product from "../components/Product";
+import Product from "../components/Product.js";
+import Message from "../components/Message.js";
+import Loader from "../components/Loader.js";
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
-    const productList = useSelector((state) => state.productListReducer);
+    const productList = useSelector((state) => state.productList);
     const { loading, error, products } = productList;
 
     useEffect(() => {
@@ -17,7 +19,7 @@ const HomeScreen = () => {
     return (
         <div>
             <h1>Latest Products</h1>
-            {loading ? (<h2>Loading...</h2>) : error ? (<h3>{error}</h3>) :
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
                 <Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
