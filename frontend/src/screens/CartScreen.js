d70ea3c30c4fb2da13bc0174fbde9ce0b87c2f88
Prakash Das <prakashindi01@gmail.com>
2021-05-02 18:41:22 +0530

    backend user authentication - user login creation

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -12,7 +12,6 @@ const CartScreen = ({ match, location, history }) => {
 	const dispatch = useDispatch();
 	const cart = useSelector(state => state.cart);
 	const { cartItems } = cart;
-	console.log('cartItems: ', cartItems);
 	useEffect(() => {
 		if (productId) {
 			dispatch(addToCart(productId, qty));
