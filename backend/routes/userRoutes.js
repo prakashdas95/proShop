5edb651e84a92e2298efafa6128c16f1b92b0630
Prakash Das <prakashindi01@gmail.com>
2021-05-15 19:42:06 +0530

    user remove - admin route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userController.js';
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deletUser } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile);
-router.route('/profile').put(protect, updateUserProfile);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deletUser);
 
 export default router;
